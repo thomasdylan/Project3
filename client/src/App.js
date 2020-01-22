4b59eb51fe416b8ac4b4e80f1a7d5058ec63f17e
Dylan Thomas <thomasdylan76@gmail.com>
2020-01-21 19:05:56 -0700

    auth0 sign in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return( 
+      <div className="spinner-border text-primary" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
     );
   }
+  return (
+    <div className="App">
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,28 @@
+// src/components/NavBar.js
+
+import React from "react";
+import {useAuth0} from "../react-auth0-spa";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
+
+    return (
+        <div>
+            {!isAuthenticated && (
+                <button type="button" className="btn btn-light" onClick={() => loginWithRedirect({})}>Log in</button>
+            )}
+
+            {isAuthenticated && <button type="button" className="btn btn-light" onClick={() => logout()}>Log out</button>}
+
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>&nbsp;
+                    <Link to="/profile">Profile</Link>
+                </span>
+            )}
+        </div>
+    );
+};
+
+export default NavBar;
