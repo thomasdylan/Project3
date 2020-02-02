230075b5e660953a7d01501cc9d4aaf4ed7d7487
Dylan Thomas <thomasdylan76@gmail.com>
2020-02-02 09:40:43 -0700

    added adsense

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import { useAuth0 } from "../react-auth0-spa";
 import { Row, Col, Button } from 'reactstrap';
 import GoalList from './GoalList';
 import GoalForm from './GoalForm';
+import AdSense from 'react-adsense';
 
 export default function Dashboard() {
     const { user } = useAuth0();
@@ -11,9 +12,10 @@ export default function Dashboard() {
         <div className= "container">
             <Row>
                 <Col sm='3'>
-                    <Button outline color="primary">
-                        Create Goal
-                    </Button>
+                    <AdSense.Google
+                        client='ca-pub-1423704383315423'
+                        slot='8434602954'
+                    />
                 </Col>
                 <Col sm='6'>
                     <GoalForm  newGoalAdded={setNewGoal}/>
